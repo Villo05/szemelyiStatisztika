95e36f985cc268f326f74a91f5065825c9cbacab
Villo05 <nemethvillo.2005@gmail.com>
2026-09-10 14:44:23 +0200

    alap

---
+++ src/main/SzemelyiStatisztika.java
@@ -0,0 +1,15 @@
+package main;
+
+public class SzemelyiStatisztika {
+
+    public static void main(String[] args) {
+        String nev;
+        int szulEv;
+        
+        /* metódusok: 
+        köszöntés
+        életkor: 2026 - 
+        nyugdíjig hátralévő évek száma*/
+    }
+    
+}
